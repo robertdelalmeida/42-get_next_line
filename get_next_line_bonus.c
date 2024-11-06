4f832e39bc17f8326ad53571d65a61f4fb32a32f
Roberto Del Franco <rdel-fra@c3r13p3.42sp.org.br>
2024-11-06 16:25:05 -0300

    finishing bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdel-fra <rdel-fra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:41:26 by marvin            #+#    #+#             */
-/*   Updated: 2024/11/06 16:18:48 by rdel-fra         ###   ########.fr       */
+/*   Updated: 2024/11/06 16:16:36 by rdel-fra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,27 @@
 
 char	*get_next_line(int fd)
 {
-	static char		*all_read;
+	static char		*all_read[1024];
 	char			*buffer;
 	char			*line;
 	char			*temp;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!all_read)
-		all_read = ft_strdup("");
+	if (all_read[fd] == NULL)
+		all_read[fd] = ft_strdup("");
 	buffer = ft_calloc((BUFFER_SIZE + 1), 1);
 	if (!buffer)
-		return (free(all_read), all_read = NULL);
-	all_read = ft_get_buffer(fd, all_read, buffer);
-	if (all_read == NULL)
+		return (free(all_read[fd]), all_read[fd] = NULL);
+	all_read[fd] = ft_get_buffer(fd, &all_read[fd][0], buffer);
+	if (all_read[fd] == NULL)
 		return (NULL);
-	line = ft_getline(all_read);
-	temp = ft_strdup(all_read + ft_strlen(line));
-	free(all_read);
-	all_read = temp;
+	line = ft_getline(&all_read[fd][0]);
+	temp = ft_strdup(&all_read[fd][0] + ft_strlen(line));
+	free(all_read[fd]);
+	all_read[fd] = temp;
 	if (!line)
-		return (free(line), free(all_read), all_read = NULL);
+		return (free(line), free(all_read[fd]), all_read[fd] = NULL);
 	return (line);
 }
 
@@ -97,7 +97,7 @@ char	*ft_getline(char *all_read)
 
 // 	printf("\nFIRST TEST\n");
 // 	fd = open("text copy.txt", O_RDONLY);
-// 	while ((line = get_next_line(fd)) != NULL)
+// 	while ((line = get_next_line(fd)) != NULL && i < 30)
 // 	{
 // 		printf("call %d is:%s", i, line);
 // 		free(line);
@@ -109,7 +109,7 @@ char	*ft_getline(char *all_read)
 // 	printf("\nSECOND TEST (with empty lines)\n");
 // 	i = 1;
 // 	fd = open("text jump line.txt", O_RDONLY);
-// 	while ((line = get_next_line(fd)) != NULL)
+// 	while ((line = get_next_line(fd)) != NULL && i < 30)
 // 	{
 // 		if (ft_strchr(line, '\n') == 0)
 // 			printf("call %d is:%s\n", i, line);
